67b40bcc141ab46514d88ca68aa900cc35cabc61
代正宏 <dzh@xuexiyou.cn>
2020-02-06 19:02:31 +0800

    mangguoTV

---
+++ pages/my/my.js
@@ -0,0 +1,33 @@
+
+
+Page({
+    data: {
+        userInfo: {
+            nickName: '123',
+            avatarUrl: '../../image/code.png'
+        }
+    },
+
+    getUserInfo () {
+        let that = this;
+        wx.getUserInfo({
+            success (res) {
+                console.log(res)
+                that.setData({ userInfo: res.userInfo })
+            }
+        })
+    },
+
+    onLoad (params) {
+        wx.login({
+            success (res) {
+                if (res.code) {
+                    console.log('登录成功！' + res.code)
+                } else {
+                    console.error('获取用户登录态失败！' + res.errMsg)
+                }
+            }
+        })
+    }
+
+})
